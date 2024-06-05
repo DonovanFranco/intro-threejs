ccbb7f7420feeeb21e391e0b74914123abb6a89d
DonovanFranco <161343179+DonovanFranco@users.noreply.github.com>
2024-06-04 19:39:26 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,32 @@
 import * as THREE from './three.js';
 
-//Crear una escena
 const scene = new THREE.Scene();
-
-//Crea una camara
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-//Crea un renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 
-//Crea el documento HTML para visualizar la escena 3D
+//crea el elemento html para visualizar la escena 3d
 document.body.appendChild( renderer.domElement );
 
-//Dibujar un cuboide
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//dibujar un cuboide
+const geometry = new THREE.BoxGeometry( 2, 1, 3 );
 
-//Crea un material para la geometria
-const material = new THREE.MeshBasicMaterial( { color: 0x09baa6 } );
-
-//Dibuja la geometria con el materia
+//crea un material para la geometria 
+const material = new THREE.MeshBasicMaterial( { color: 0x2569b8 } );
+//dibuja la geometria con el material
 const cube = new THREE.Mesh( geometry, material );
-
-//Agrega el cubo a la escena
+//agrega el cubo a la escena
 scene.add( cube );
-
+//
 camera.position.z = 5;
 
 function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-	cube.rotation.z += 0.01;
+    cube.rotation.z += 0.01;
 
 	renderer.render( scene, camera );
 
